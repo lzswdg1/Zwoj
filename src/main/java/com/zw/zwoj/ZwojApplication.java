b6a89643a534c335790e4a2bdbd7810e39291e90
zw <15877035+zhang-wei-top1@user.noreply.gitee.com>
2025-09-30 18:04:16 +0800

    第4次提交

--- src/main/java/com/zw/zwoj/ZwojApplication.java
+++ src/main/java/com/zw/zwoj/ZwojApplication.java
@@ -1,9 +1,13 @@
 package com.zw.zwoj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("com.zw.zwoj.mapper")
+@EnableScheduling
 public class ZwojApplication {
 
     public static void main(String[] args) {
